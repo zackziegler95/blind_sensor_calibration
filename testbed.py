5141830f109bd9266a1b466ed73741dc246e408d
gkehne <greg.kehne@gmail.com>
2021-04-28 16:48:55 -0400

    new algos for confidences

--- testbed.py
+++ testbed.py
@@ -6,6 +6,7 @@ import pandas as pd
 import ast
 import numpy as np
 import matplotlib.pyplot as plt
+from random import choice
 
 def contains_all_indices(l, n):
     sorted_l = []
@@ -15,7 +16,7 @@ def contains_all_indices(l, n):
     return sorted_l == list(range(n))
 
 class Simulator():
-    def __init__(self, data_dir, num_sensors, size, start_time, delta=1, max_sec=-1, subsample_freq=1, sensor_proximity_events_file=None):
+    def __init__(self, data_dir, num_sensors, particle_size, start_time, delta=1, max_sec=-1, subsample_freq=1, sensor_proximity_events_file=None):
         # delta is the step time in seconds
         self.delta = delta
         self.num_sensors = num_sensors
@@ -29,6 +30,7 @@ class Simulator():
         self.prox_groups = groups
 
         self.all_data = []
+        # populate all_data from CSVs
         for i in range(1, num_sensors+1):
             data = pd.read_csv(f'{data_dir}/Sensor{i}.csv')
             data = data.iloc[::subsample_freq, :]
@@ -45,7 +47,7 @@ class Simulator():
             times = times[cond]
 
 
-            particle_density = data[size]
+            particle_density = data[particle_size]
             particle_density = particle_density[cond]
             self.all_data.append((times, particle_density))
 
@@ -55,7 +57,6 @@ class Simulator():
 
         # update neighbors based on prox_events
         new_prox_groupings = self.prox_groups[(self.prox_times >= t) & (self.prox_times < endt)]
-        
         if len(new_prox_groupings) > 0:
             new_prox_groupings = new_prox_groupings.iloc[-1]
             assert contains_all_indices(new_prox_groupings, self.num_sensors)
@@ -65,26 +66,60 @@ class Simulator():
                     if i in subgroup:
                         self.neighbors[i] = [subgroup_item for subgroup_item in subgroup if subgroup_item != i]
                         break
-        
+
         for i in range(self.num_sensors):
+            # skip sensors which have run out of data
             if self.done[i]:
                 continue
 
-
-            # update values and confidences based on data
-
+            # update values and times based on newly arrived data
             times_i = self.all_data[i][0]
             vals_i = self.all_data[i][1]
             new_data = vals_i[(times_i > t) & (times_i <= endt)]
 
+            # update confidences: "Simple MW update"
             if len(new_data) > 0:
+                adjustment_rate = 0.1
                 self.values[i] = new_data.iloc[0]
-                if self.values[i] < 100: # 100 ug/m3
-                    self.confidences[i] = 11.25
-                elif self.values[i] < 1000: # 1000 ug/m3
-                    self.confidences[i] = self.values[i]*0.1125
-                else:
-                    self.confidences[i] = 999999 # no confidence at all!
+                # randomly choose other sensor that isn't done
+                other_live_sensor_indices = [j for j in range(self.num_sensors) if not j==i and not self.done[j]]
+                if len(other_live_sensor_indices) == 0:
+                    continue
+                j = choice(other_live_sensor_indices)
+                # take difference in values
+                val_difference = self.values[i] - self.values[j]
+                # update both (just the one?) confidences accordingly
+                # sum_of_confidences = self.confidences[i] + self.confidences[j]
+                multiplicative_adjustment = (1 + adjustment_rate * (abs(val_difference)/self.confidences[i] - 1))
+                self.confidences[i] = self.confidences[i] * multiplicative_adjustment
+                # self.confidences[j] = self.confidences[j] * multiplicative_adjustment
+
+            # update confidences: "Complex" MW update
+            # if len(new_data) > 0:
+            #     adjustment_rate = 0.1
+            #     self.values[i] = new_data.iloc[0]
+            #     # randomly choose other sensor that isn't done
+            #     other_live_sensor_indices = [j for j in range(self.num_sensors) if not j==i and not self.done[j]]
+            #     if len(other_live_sensor_indices) == 0:
+            #         continue
+            #     j = choice(other_live_sensor_indices)
+            #     # take difference in values
+            #     val_difference = self.values[i] - self.values[j]
+            #     # update both (just the one?) confidences accordingly
+            #     sum_of_confidences = self.confidences[i] + self.confidences[j]
+            #     multiplicative_adjustment = (1 + adjustment_rate * (abs(val_difference)/sum_of_confidences - 1))
+            #     self.confidences[i] = self.confidences[i] * multiplicative_adjustment
+            #     self.confidences[j] = self.confidences[j] * multiplicative_adjustment
+
+            # "Baseline" approach
+            # if len(new_data) > 0:
+            #     self.values[i] = new_data.iloc[0]
+            #     if self.values[i] < 100: # 100 ug/m3
+            #         self.confidences[i] = 11.25
+            #     elif self.values[i] < 1000: # 1000 ug/m3
+            #         self.confidences[i] = self.values[i]*0.1125
+            #     else:
+            #         self.confidences[i] = 999999 # no confidence at all!
 
             remaining_times = sum(times_i > endt)
             if remaining_times == 0:
@@ -103,8 +138,10 @@ class Simulator():
     def simulate(self):
         # all_data is a list of pandas dataframes
         t = 0
+        # stores current vals and confidences:
         self.values = [0 for _ in range(self.num_sensors)]
         self.confidences = [1 for _ in range(self.num_sensors)]
+        # keeps track of whether sensor has run out of data:
         self.done = [False for _ in range(self.num_sensors)]
         self.neighbors = [[] for _ in range(self.num_sensors)]
 
@@ -156,28 +193,28 @@ def group_results_by_proximity(results, prox_times, prox_groups):
         current_grouping.append([times[i], values[i], confs[i]])
 
     all_groupings.append(current_grouping)
-    all_colors.append(get_colors_from_group(prox_groups[last_prox_index], num_sensors))           
+    all_colors.append(get_colors_from_group(prox_groups[last_prox_index], num_sensors))
 
     return all_groupings, all_colors
 
 
 def main():
     # parameters
-    runname = 'baseline'
+    runname = 'simple_mult_updates'
     data_dir = 'data_tscorrect'
     plot_dir = 'plots_experiment'
     sim_results_dir = 'saved_sims'
     num_sensors = 4
-    size = 'PM25'
+    particle_size = 'PM25'
     subsample_freq = 1 # i.e. only take every <value>th sample from each sensor
     max_sec = 60*630 #60*630
-    sensor_proximity_events_file = 'proximity_events/4_occasional_drop.csv'
+    sensor_proximity_events_file = 'proximity_events/4_constant.csv'
     plot_only = False
     color_by_group = False
 
     os.makedirs(plot_dir, exist_ok=True)
     os.makedirs(sim_results_dir, exist_ok=True)
-    
+
     # Run simulation
     if plot_only:
         with open(f'{sim_results_dir}/{runname}.pkl', 'rb') as f:
@@ -186,8 +223,8 @@ def main():
         start_time = '04/11/2021 01:21:17 PM'
         start_time = pd.to_datetime(start_time, infer_datetime_format=True)
 
-        sim = Simulator(data_dir, num_sensors, size, start_time, max_sec=max_sec, subsample_freq=subsample_freq, sensor_proximity_events_file=sensor_proximity_events_file)
-        
+        sim = Simulator(data_dir, num_sensors, particle_size, start_time, max_sec=max_sec, subsample_freq=subsample_freq, sensor_proximity_events_file=sensor_proximity_events_file)
+
         combined_results = list(sim.simulate())
         prox_times = sim.prox_times
         prox_groups = sim.prox_groups
@@ -216,17 +253,17 @@ def main():
             plt.fill_between(times, vals_i-confs_i, vals_i+confs_i, alpha=0.2, color=color)
 
     plt.xlabel('Time (seconds)')
-    plt.ylabel(f'{size} (ug/m3)')
+    plt.ylabel(f'{particle_size} (ug/m3)')
     plt.ylim([0, 50])
 
     if not color_by_group:
         plt.legend()
-    plt.savefig(f'{plot_dir}/{runname}_all_data_{size}.pdf')
-    plt.savefig(f'{plot_dir}/{runname}_all_data_{size}.png')
+    plt.savefig(f'{plot_dir}/{runname}_all_data_{particle_size}.pdf')
+    plt.savefig(f'{plot_dir}/{runname}_all_data_{particle_size}.png')
 
     plt.ylim([0, 500])
-    plt.savefig(f'{plot_dir}/{runname}_all_data_{size}_largescale.pdf')
-    plt.savefig(f'{plot_dir}/{runname}_all_data_{size}_largescale.png')
+    plt.savefig(f'{plot_dir}/{runname}_all_data_{particle_size}_largescale.pdf')
+    plt.savefig(f'{plot_dir}/{runname}_all_data_{particle_size}_largescale.png')
 
 if __name__ == '__main__':
     main()
